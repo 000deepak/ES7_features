f41539bf8ca0622dbda4202aa7e40fc25edcefd8
000deepak <kumbhardeepak000@gmail.com>
2021-12-15 15:37:31 +0530

    ES7 Features

---
+++ ES7.js
@@ -0,0 +1,6 @@
+//Array.prototype.includes()
+//returns true if array contains the value, otherwise it returns false.
+//handles NaN better than Array.prototype.indexOf(). 
+//If the array contains NaN, then indexOf() does not return a correct index while searching for NaN.
+
+//Exponentiation Operator
